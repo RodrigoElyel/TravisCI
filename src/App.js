3cb0934bb3b968cc83b7367faab0f80ea2fc75e6
RodrigoElyel <rodrigoelyelcb@gmail.com>
2021-06-06 13:40:55 -0300

    Add Travis

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Iniciar exemplo aqui.
         </p>
         <a
           className="App-link"
