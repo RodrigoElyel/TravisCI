af9858e9f7856c51b7f089609deab50bf273a0ae
RodrigoElyel <rodrigoelyelcb@gmail.com>
2021-06-06 13:54:19 -0300

    fix problem

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Problemas resolvidos.
+          Travis CI
+        </p>
+        <p>
+          Travis CI
+        </p>
+        <p>
+          Travis CI
         </p>
         <a
           className="App-link"
